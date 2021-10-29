6161106b64a5bd6c37e04c24eabd7e82c1b31397
HYPER <88543251+HYPER-OP@users.noreply.github.com>
2021-10-29 14:55:04 +0530

    Update stickers.py

--- HYPERX/modules/stickers.py
+++ HYPERX/modules/stickers.py
@@ -400,7 +400,7 @@ def makepack_internal(
             success = context.bot.create_new_sticker_set(
                 user.id,
                 packname,
-                f"{name}s kang pack" + extra_version,
+                f"{name}s 𝙺𝙰𝙽𝙶 𝙿𝙰𝙲𝙺" + extra_version,
                 png_sticker=png_sticker,
                 emojis=emoji,
             )
@@ -408,7 +408,7 @@ def makepack_internal(
             success = context.bot.create_new_sticker_set(
                 user.id,
                 packname,
-                f"{name}s animated kang pack" + extra_version,
+                f"{name}s animated 𝙺𝙰𝙽𝙶 𝙿𝙰𝙲𝙺" + extra_version,
                 tgs_sticker=tgs_sticker,
                 emojis=emoji,
             )
