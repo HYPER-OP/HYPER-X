adc5d5a4fc4d271dcecf42d05590b832ba6a1d16
Shinchan7222 <83214906+Shinchan7222@users.noreply.github.com>
2021-11-10 23:50:32 +0530

    Update __init__.py

--- HYPERX/__init__.py
+++ HYPERX/__init__.py
@@ -201,7 +201,7 @@ updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
 telethn = TelegramClient("innexia", API_ID, API_HASH)
 pbot = Client("HYPERX", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 dispatcher = updater.dispatcher
-ubot = TelegramClient(StringSession(STRING_SESSION), APP_ID, APP_HASH)
+ubot = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH)
 
 DRAGONS = list(DRAGONS) + list(DEV_USERS)
 DEV_USERS = list(DEV_USERS)
