81adeb03f500e9d1f5193a94e6009c680cca4cfd
HYPER <88543251+HYPER-OP@users.noreply.github.com>
2021-12-12 01:12:09 +0530

    Update userinfo_sql.py

--- HYPERX/modules/sql/userinfo_sql.py
+++ HYPERX/modules/sql/userinfo_sql.py
@@ -1,12 +1,12 @@
 import threading
 
 from HYPERX.modules.sql import BASE, SESSION
-from sqlalchemy import Column, Integer, UnicodeText
+from sqlalchemy import Column, BigInteger, UnicodeText
 
 
 class UserInfo(BASE):
     __tablename__ = "userinfo"
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(BigInteger, primary_key=True)
     info = Column(UnicodeText)
 
     def __init__(self, user_id, info):
