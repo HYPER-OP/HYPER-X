c4e683f13773f64fddd268de8427d5e1f13bafb0
HYPER <88543251+HYPER-OP@users.noreply.github.com>
2021-12-12 01:40:49 +0530

    Update afk_sql.py

--- HYPERX/modules/sql/afk_sql.py
+++ HYPERX/modules/sql/afk_sql.py
@@ -1,7 +1,7 @@
 import threading
 
 from HYPERX.modules.sql import BASE, SESSION
-from sqlalchemy import Boolean, Column, Integer, UnicodeText
+from sqlalchemy import Boolean, Column, BigInteger, UnicodeText
 
 
 class AFK(BASE):
