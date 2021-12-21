845b644b1e44590f13130e7ef20363b4aa34c3c4
HYPER <88543251+HYPER-OP@users.noreply.github.com>
2021-12-22 03:00:52 +0530

    Update admin.py

--- HYPERX/modules/admin.py
+++ HYPERX/modules/admin.py
@@ -793,7 +793,7 @@ dispatcher.add_handler(SETCHAT_TITLE_HANDLER)
 dispatcher.add_handler(SETSTICKET_HANDLER)
 dispatcher.add_handler(SETDESC_HANDLER)
 
-__mod_name__ = "ᴀᴅᴍɪɴ"
+__mod_name__ = "ADMIN"
 __command_list__ = [
     "adminlist",
     "admins",
