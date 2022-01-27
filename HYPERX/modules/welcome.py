66d5b8ed7ce80a51d67c24af17c19bcf13643508
HYPER <88543251+HYPER-OP@users.noreply.github.com>
2022-01-27 20:55:07 +0530

    Update welcome.py

--- HYPERX/modules/welcome.py
+++ HYPERX/modules/welcome.py
@@ -553,7 +553,7 @@ def left_member(update: Update, context: CallbackContext):
             # Give the devs a special goodbye
             elif left_mem.id in DEV_USERS:
                 update.effective_message.reply_text(
-                    "See you later at the Hero's Association!",
+                    "See you later at the ʙɪᴏɴɪᴄ sᴜᴘᴘᴏʀᴛ!",
                     reply_to_message_id=reply,
                 )
                 return
