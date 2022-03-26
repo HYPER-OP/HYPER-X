a93114a19d5c6835f0f6d42d27e88534ed390257
cyraxop123 <77997274+cyraxop123@users.noreply.github.com>
2022-03-27 01:16:37 +0530

    Update __init__.py

--- HYPERX/__init__.py
+++ HYPERX/__init__.py
@@ -16,11 +16,7 @@ from aiohttp import ClientSession
 StartTime = time.time()
 
 # enable logging
-#logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
-    level=logging.INFO,
-)
+
 
 LOGGER = logging.getLogger(__name__)
 
